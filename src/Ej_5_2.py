a6fdda3c670ca223dc5b720463fde3ede8b1c675
Ejercicios_DAW_Python <aarrieta@iesrafaelalberti.es>
2023-11-07 18:07:07 +0100

    Ejercicios y Tests 1 al 5

--- src/Ej_5_2.py
+++ src/Ej_5_2.py
@@ -9,9 +9,29 @@ Al final debe mostrar también el número total de créditos del curso.
 """
 
 
-def function(example: str) -> str:
-    return None
+def mensaje(curso: dict) -> str:
+    mensaje = ""
+    claves = list(curso.keys())
+    for asignatura in claves:
+        mensaje += f"{asignatura} tiene {curso.get(asignatura)} créditos\n"
+    return mensaje[:-1]
+
+
+def total_creditos(curso: dict) -> str:
+    total = 0
+    creditos = list(curso.values())
+    for suma in creditos:
+        total += suma
+    mensaje = f"Créditos totales del curso: {total}"
+    return mensaje
 
 
 if __name__ == "__main__":
-    ""
+    # Entrada
+    creditos_del_curso = {'Matemáticas': 6, 'Física': 4, 'Química': 5}
+    # Proceso
+    mensaje = mensaje(creditos_del_curso)
+    creditos = total_creditos(creditos_del_curso)
+    # Salida
+    print(f"{mensaje}\n{creditos}")
+
--- test/test_1_2.py
+++ test/test_1_2.py
@@ -1,13 +1,27 @@
 import pytest
-from src.Ej_1_2 import function
+from src.Ej_1_2 import control, simbolo
 
 
 @pytest.mark.parametrize(
-    "inMensaje, outMensaje",
+    "inClave, inDiccionario, outFallo",
     [
-        ("Entrada1", "Salida1"),
-        ("Entrada2", "Salida2")
+        ("Libra", {'Euro': '€', 'Dollar': '$', 'Yen': '¥'}, 1),
+        ("Euro", {'Euro': '€', 'Dollar': '$', 'Yen': '¥'}, 0),
+        ("Rupia", {'Euro': '€', 'Dollar': '$', 'Yen': '¥'}, 1),
+        ("Yen", {'Euro': '€', 'Dollar': '$', 'Yen': '¥'}, 0)
     ]
 )
-def test_function(inMensaje, outMensaje):
-    assert function(inMensaje) == outMensaje
+def test_control(inClave, inDiccionario, outFallo):
+    assert control(inClave, inDiccionario) == outFallo
+
+
+@pytest.mark.parametrize(
+    "inClave, inDiccionario, outDivisa",
+    [
+        ("Libra", {'Euro': '€', 'Dollar': '$', 'Yen': '¥'}, None),
+        ("Euro", {'Euro': '€', 'Dollar': '$', 'Yen': '¥'}, '€'),
+        ("Yen", {'Euro': '€', 'Dollar': '$', 'Yen': '¥'}, '¥'),
+    ]
+)
+def test_simbolo(inClave, inDiccionario, outDivisa):
+    assert simbolo(inClave, inDiccionario) == outDivisa
