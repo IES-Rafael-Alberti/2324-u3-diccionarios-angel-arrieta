525048b1216eaccd8b14dc26cfd543d35cd2994c
Ejercicios_DAW_Python <aarrieta@iesrafaelalberti.es>
2023-11-10 16:33:46 +0100

    Ejercicio y Test 11

--- src/Ej_11_2.py
+++ src/Ej_11_2.py
@@ -23,9 +23,45 @@ Es decir, un diccionario como el siguiente
 """
 
 
-def function(example: str) -> str:
-    return None
+def dividir(cadena_bruta: str) -> tuple:
+    listado = cadena_bruta.split("\n")
+    cabecera = listado.pop(0)
+    return cabecera, listado
+
+
+def personas(listado: list) -> list:
+    informacion = []
+    for linea in listado:
+        datos = linea.split(";")
+        informacion.append(datos)
+    return informacion
+
+
+def diccionario(asociacion: list, info_personas: list) -> dict:
+    diccionario = {}
+    for persona in info_personas:
+        individuo = {}
+        for valor in persona:
+            if valor == persona[0]:
+                nif = persona[0]
+            else:
+                individuo.update({asociacion[persona.index(valor)]: valor})
+        diccionario.update({nif: individuo})
+    return diccionario
 
 
 if __name__ == "__main__":
-    ""
+    # Entrada
+    directorio = ("nif;nombre;email;teléfono;descuento\n"
+                  "01234567L;Luis González;luisgonzalez@mail.com;656343576;12.5\n"
+                  "71476342J;Macarena Ramírez;macarena@mail.com;692839321;8\n"
+                  "63823376M;Juan José Martínez;juanjo@mail.com;664888233;5.2\n"
+                  "98376547F;Carmen Sánchez;carmen@mail.com;667677855;15.7")
+    # Proceso
+    referencia, directorio_dividido = dividir(directorio)
+    claves = referencia.split(";")
+    datos_personas = personas(directorio_dividido)
+    clientes = diccionario(claves, datos_personas)
+    # Salida
+    print(clientes)
+
